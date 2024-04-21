879879e6584833d836b3318585297ef31b09c17a
Mateusz Paulus <paulus.m.mateusz@gmail.com>
2024-04-21 13:05:35 +0200

    FEATURE: Pass data through component using event binding and property binding

--- basic-recipes-app/src/app/app.component.ts
+++ basic-recipes-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'basic-recipes-app';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  };
 }
