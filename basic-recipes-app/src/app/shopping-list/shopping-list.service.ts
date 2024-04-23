eb2593899897e1d830ec0658a0f900bcfc7bbf8d
Mateusz Paulus <paulus.m.mateusz@gmail.com>
2024-04-23 12:54:25 +0200

    FEATURE: Use observables subject instead of emitter

--- basic-recipes-app/src/app/shopping-list/shopping-list.service.ts
+++ basic-recipes-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,10 @@
-import {EventEmitter} from "@angular/core";
+import {Subject} from "rxjs";
+
 import {IngredientModel} from "../shared/ingredient.model";
 
 export class ShoppingListService {
-  ingredientsChange = new EventEmitter<IngredientModel[]>();
+  // ingredientsChange = new EventEmitter<IngredientModel[]>();
+  ingredientsChange = new Subject<IngredientModel[]>();
 
   private ingredients: IngredientModel[] = [
     new IngredientModel('Apples', 5),
@@ -15,7 +17,8 @@ export class ShoppingListService {
 
   addIngredient(ingredient: IngredientModel) {
     this.ingredients.push(ingredient);
-    this.ingredientsChange.emit(this.ingredients.slice());
+    // this.ingredientsChange.emit(this.ingredients.slice());
+    this.ingredientsChange.next(this.ingredients.slice());
   };
 
   addIngredients(ingredients: IngredientModel[]) {
@@ -23,6 +26,7 @@ export class ShoppingListService {
     //   this.addIngredient(ingredient);
     // }
     this.ingredients.push(...ingredients);
-    this.ingredientsChange.emit(this.ingredients.slice());
+    // this.ingredientsChange.emit(this.ingredients.slice());
+    this.ingredientsChange.next(this.ingredients.slice());
   }
 }
