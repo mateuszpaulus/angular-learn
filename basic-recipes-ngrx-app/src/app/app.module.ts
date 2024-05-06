0b4c8c09031c380fe2b41b0f7bf29a788caefcb6
Mateusz Paulus <paulus.m.mateusz@gmail.com>
2024-05-06 22:37:21 +0200

    FEATURE: Manage use login effect with ngrx

--- basic-recipes-ngrx-app/src/app/app.module.ts
+++ basic-recipes-ngrx-app/src/app/app.module.ts
@@ -10,6 +10,8 @@ import {AppRoutingModule} from "./app-routing.module";
 import {SharedModule} from "./shared/shared.module";
 import {CoreModule} from "./core.module";
 import * as fromApp from './store/app.reducer'
+import {EffectsModule} from "@ngrx/effects";
+import {AuthEffects} from "./auth/store/auth.effects";
 
 @NgModule({
   declarations: [
@@ -23,7 +25,8 @@ import * as fromApp from './store/app.reducer'
     HttpClientModule,
     SharedModule,
     CoreModule,
-    StoreModule.forRoot(fromApp.appReducer)
+    StoreModule.forRoot(fromApp.appReducer),
+    EffectsModule.forRoot([AuthEffects])
   ],
   providers: [],
   bootstrap: [AppComponent],
